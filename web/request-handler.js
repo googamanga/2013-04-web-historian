f7117bdcbe8b025d8943fabe71c409bb0e1ffc47
Hack Reactor Students <github@hackreactor.com>
2013-05-18 11:17:14 -0700

    send index file with css to client

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,5 +1,33 @@
 exports.datadir = __dirname + "data/sites.txt"; // tests will need to override this.
+var fs = require('fs');
 
-exports.handleRequest = function (req, res) {
+var defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GETS, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 //seconds
+};
+
+exports.handleRequest = function (request, response) {
   console.log(exports.datadir);
+  if (request.url === '/' || request.url === '/index.html') {
+    fs.readFile('./public/index.html','utf8',function (err, data) {
+      if (err) throw err;
+      returnCode = 200;
+      defaultCorsHeaders["Content-Type"] = "text/html";
+      response.writeHead(returnCode, defaultCorsHeaders);
+      response.end(data);
+      return;
+    });
+  }
+  if (request.url === '/styles.css') {
+    fs.readFile('./public/styles.css','utf8',function (err, data) {
+      if (err) throw err;
+      returnCode = 200;
+      defaultCorsHeaders["Content-Type"] = "text/css";
+      response.writeHead(returnCode, defaultCorsHeaders);
+      response.end(data);
+      return;
+    });
+  }
 };
